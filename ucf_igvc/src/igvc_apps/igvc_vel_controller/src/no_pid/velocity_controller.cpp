dedf88882af9fd4f2d4be9473161adc8c56fb373
Thomas Watters <thomaswatters@gmail.com>
2015-05-18 13:17:17 -0400

    blah

--- ucf_igvc/src/igvc_apps/igvc_vel_controller/src/no_pid/velocity_controller.cpp
+++ ucf_igvc/src/igvc_apps/igvc_vel_controller/src/no_pid/velocity_controller.cpp
@@ -35,7 +35,7 @@ namespace igvc
 
     VelocityController::VelocityController(std::string cmd_topic, std::vector<std::string> namespaces,
                                            double wheel_radius, double base_radius)
-            : nh_(), base_radius_(base_radius), wheel_radius_(wheel_radius), counter_(0)
+            : nh_(), base_radius_(base_radius), wheel_radius_(wheel_radius), counter(0)
     {
         right_motor_pub_ = nh_.advertise<roboteq_msgs::Command>(namespaces[0] + "/cmd", 1);
         left_motor_pub_ = nh_.advertise<roboteq_msgs::Command>(namespaces[1] + "/cmd", 1);
